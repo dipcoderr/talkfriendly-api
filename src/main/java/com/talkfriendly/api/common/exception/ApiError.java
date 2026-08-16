1d8f4840eb7100a0eb853415148b070d0b7b8551
Dipanshu Mishra <148424835+dipcoderr@users.noreply.github.com>
2026-08-16 15:14:36 +0530

    Add src/main/java/com/talkfriendly/api/common/exception/ApiError.java

---
+++ src/main/java/com/talkfriendly/api/common/exception/ApiError.java
@@ -0,0 +1,4 @@
+package com.talkfriendly.api.common.exception;
+import java.time.Instant;
+import java.util.Map;
+public record ApiError(Instant timestamp, int status, String error, String message, Map<String,String> fieldErrors) { }
