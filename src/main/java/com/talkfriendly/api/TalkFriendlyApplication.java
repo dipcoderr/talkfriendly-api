5af485e1485e9984274c99a134b8bcde2ddfea41
Dipanshu Mishra <148424835+dipcoderr@users.noreply.github.com>
2026-08-16 15:15:05 +0530

    Add src/main/java/com/talkfriendly/api/TalkFriendlyApplication.java

---
+++ src/main/java/com/talkfriendly/api/TalkFriendlyApplication.java
@@ -0,0 +1,9 @@
+package com.talkfriendly.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkFriendlyApplication {
+    public static void main(String[] args) { SpringApplication.run(TalkFriendlyApplication.class, args); }
+}
